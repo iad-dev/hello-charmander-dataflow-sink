c94ca768bda10310d2da65e9dbd3f0497adf5ab9
Ian Fisher <loktar@gmail.com>
2016-09-28 16:54:39 -0700

    Dataflow sink created
    Signed-off-by: Heather Farley <hrfarley@gmail.com>

---
+++ src/main/java/io/pivotal/configuration/SinkConfiguration.java
@@ -0,0 +1,17 @@
+package io.pivotal.configuration;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.integration.annotation.ServiceActivator;
+
+@EnableBinding(Sink.class)
+public class SinkConfiguration {
+    private static final Log logger = LogFactory.getLog(SinkConfiguration.class);
+
+    @ServiceActivator(inputChannel=Sink.INPUT)
+    public void loggerSink(Object payload) {
+        logger.info("Hello, Charmander. The time is: " + payload);
+    }
+}
